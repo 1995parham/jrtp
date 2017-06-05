c236c046352bac2d3c3540a309b9bf593464fdac
Parham Alvani <parham.alvani@gmail.com>
2017-06-05 19:19:40 +0430

    I think c is better at last :P

---
+++ src/packet.c
@@ -0,0 +1,13 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : packet.c
+ *
+ * [] Creation Date : 05-06-2017
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2017 Parham Alvani.
+*/
---
+++ src/packet.h
@@ -0,0 +1,13 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : packet.h
+ *
+ * [] Creation Date : 05-06-2017
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2017 Parham Alvani.
+*/
