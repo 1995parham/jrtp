4b09fd528304b81f2635965b67ce6deb8ebc2962
Parham Alvani <parham.alvani@gmail.com>
2017-06-05 21:32:11 +0430

    Adds packet serialization.

--- src/packet.c
+++ src/packet.c
@@ -11,3 +11,32 @@
 /*
  * Copyright (c) 2017 Parham Alvani.
 */
+
+
+#include "packet.h"
+
+
+int rtp_packet_serialize(const struct rtp_packet *p, uint8_t *buff)
+{
+	/* buff[0] = (V << 6 | P << 5 | X << 4 | CC) */
+	buff[0] = (2 << 6 | 0 << 5 | 0 << 4 | 0);
+	/* buff[1] = (M << 7 | PT) */
+	buff[1] = (0 << 7 | p->payload_type);
+	/* buff[2, 3] = SN */
+	buff[2] = p->sequence_number >> 8;
+	buff[3] = p->sequence_number;
+	/* buff[4, 5, 6, 7] = TS */
+	buff[4] = p->timestamp >> 24;
+	buff[5] = p->timestamp >> 16;
+	buff[6] = p->timestamp >> 8;
+	buff[7] = p->timestamp;
+	/* buff[4, 5, 6, 7] = SSRC */
+	buff[8] = p->ssrc >> 24;
+	buff[9] = p->ssrc >> 16;
+	buff[10] = p->ssrc >> 8;
+	buff[11] = p->ssrc;
+}
+
+int rtp_packet_deserialize(struct rtp_packet *p, const uint8_t *buff)
+{
+}
