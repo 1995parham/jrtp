5ef9a17b0d5eaf803e9a2523eed62fb8d5de3e96
Parham Alvani <parham.alvani@gmail.com>
2017-06-08 13:57:05 +0430

    Adds default constructor.

--- src/RTPPacket.h
+++ src/RTPPacket.h
@@ -18,13 +18,14 @@
 
 class RTPPacket {
 	public:
+		RTPPacket() : RTPPacket(nullptr, 0, 0, 0, 0) {};
 		RTPPacket(const uint8_t *payload, uint16_t sequenceNumber, uint32_t ssrc, uint32_t timestamp) : RTPPacket(payload, sequenceNumber, ssrc, timestamp, 95) {};
 		RTPPacket(const uint8_t *payload, uint16_t sequenceNumber, uint32_t ssrc) : RTPPacket(payload, sequenceNumber, ssrc, 0, 95) {};
 		RTPPacket(const uint8_t *payload, uint16_t sequenceNumber, uint32_t ssrc, uint32_t timestamp, uint8_t payloadType);
 		RTPPacket(const RTPPacket& src) = default;
 
 		int serialize(uint8_t *buff) const;
-		int deserialize(const uint8_t *buff);
+		int deserialize(const uint8_t *buff, int length);
 
 		const uint8_t *getPayload() const;
 		void setPayload(uint8_t *payload);
@@ -36,6 +37,5 @@ class RTPPacket {
 		uint32_t timestamp;
 		uint8_t payloadType;
 };
-
 
 #endif
